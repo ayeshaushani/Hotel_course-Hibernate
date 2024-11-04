2cdd0cef60b484457caa30c34e9e844af14f7825
ayeshaushani <akisasenari@gmail.com>
2024-11-04 06:51:26 +0530

    MVC done

---
+++ src/main/java/org/example/Launcher.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
+public class Launcher {
+    public TextField textfield;
+    public TextField textfield2;
+
+    public static void main(String[] args) {
+    }
+
+    public void btnSaveOnAction(ActionEvent event) {
+    }
+}
